a5b21b9a4875e36a5af3fc24d21bbf578389f7bf
Arangarajan <arangarajanmoorthy002@gmail.com>
2023-09-09 11:16:45 +0530

    new filr added

---
+++ quiz.py
@@ -0,0 +1,17 @@
+Tname=input("enter the name : ")
+
+score=0 
+q1=input("What is the full form of cpu : ")
+if q1.lower() == "central processing unit":
+    print("correct")
+    score += 1
+else:
+    print("Incorrect")
+
+q2=input("What is the full form of cpu : ")
+if q1.lower() == "central processing unit":
+    print("correct")
+    score += 1
+else:
+    print("Incorrect")  
+print(f"your name{Tname}.your score is {score} ")          
